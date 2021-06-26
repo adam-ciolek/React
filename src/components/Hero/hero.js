7621e671fd5b98f9ce7d437710307ac53d9ecf9c
Adam Ciołek <adasciolek@gmail.com>
2021-06-26 18:06:28 +0200

    Add new components

---
+++ src/components/Hero/hero.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+
+
+class Hero extends React.Component {
+  render() {
+    return (
+
+      <header className={styles.component}>
+        <h2 className={styles.title}>React Op</h2>
+        <img className={styles.image} src='https://i.postimg.cc/jj06w3Pf/space.png' />
+      </header>
+
+    )
+  }
+};
+
+export default Hero;
---
+++ src/components/List/List.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/hero';
+
+
+class List extends React.Component {
+  render() {
+    return (
+
+      <section className={styles.component}>
+        <Hero />
+      </section>
+
+    )
+  }
+}
+
+export default List;
